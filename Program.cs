50da4ab0edb0dafda5d47375286c448aa43b83ad
WATANABE Hirotada <beqpad@hotmail.com>
2023-08-03 17:11:24 +0900

    test code1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net;
+
+Console.WriteLine("Hello, World!");
+
+
+public static class Class1
+{
+    public static void Method1()
+    {
+        var protocolType = SecurityProtocolType.Ssl3;
+        Console.WriteLine(protocolType);
+    }
+
+    public static void Method2(string relativePath)
+    {
+        var path = $"{Environment.CurrentDirectory}/{relativePath}";
+        Console.WriteLine(path);
+    }
+}
